d82c210874f81d687abb022768cead21d5b03eed
lalit-1729 <lakithakare54726@gmail.com>
2021-01-23 23:35:05 +0530

    Ques1-a modified with input and pattern printing function.

--- Ques1-a.cpp
+++ Ques1-a.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+void print_pattern(int no_of_rows){
+    for(int row = 0; row<no_of_rows ; row++){
+        for(int character = 0; character<=row ; character++){
+            cout << "$";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     // run again loop
     char run_again = 'y';
@@ -14,14 +23,7 @@ int main(){
         cin >> input_num;
         cout << endl;
 
-        //implementing the logic
-        for(int row = 0; row<input_num ; row++){
-
-            for(int character = 0; character<=row ; character++){
-                cout << "$";
-            }
-            cout << endl;
-        }
+        print_pattern(input_num);
 
         //asking to run again
         cout << "Do you wanna run program again...??? (y/n) " << endl;
