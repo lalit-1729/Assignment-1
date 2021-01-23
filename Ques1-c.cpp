40d909025c60356b5a64d4cb3134459cbda3c2a3
lalit-1729 <lakithakare54726@gmail.com>
2021-01-23 23:48:16 +0530

    Ques1-c modified with take_input and pattern printing function.

--- Ques1-c.cpp
+++ Ques1-c.cpp
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 using namespace std;
 
+// to take the user input
+void take_input(int *no_of_rows){
+    cout << "Enter the no. of rows of the pattern: ";
+    cin >> *no_of_rows;
+    cout << endl;
+    return;
+}
+
+
+// function to print the pattern
+void print_pattern(int no_of_rows){
+    for(int row = 0; row < no_of_rows ; row++){
+        for(int space = 1; space < no_of_rows - row ; space++ ){
+            cout << " ";
+        }
+        for(int character = 0; character < (2*row + 1) ; character++){
+            cout << "$";
+        }
+        cout << endl;
+    }
+    return;
+}
+
 int main(){
     // run again loop
     char run_again = 'y';
@@ -9,24 +32,12 @@ int main(){
         system("cls");
 
         int input_num;
-        cout << "Enter the no. of rows of the pattern: ";
-        cin >> input_num;
 
-
-        for(int row = 0; row < input_num ; row++){
-
-            for(int space = 1; space < input_num-row ; space++ ){
-                cout << " ";
-            }
-            for(int character = 0; character < (2*row + 1) ; character++){
-                cout << "$";
-            }
-            cout << endl;
-        }
-
+        take_input(&input_num);
+        print_pattern(input_num);
 
         //asking to run again
-        cout << "Do you wanna run program again...??? (y/n) " << endl;
+        cout << "\nDo you wanna run program again...??? (y/n) " << endl;
         cin >> run_again;
         cout << endl;
     }
