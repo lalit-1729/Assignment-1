7a5084e14f39b420a00c0025cb20c3c982310563
lalit-1729 <lakithakare54726@gmail.com>
2021-01-23 23:54:50 +0530

    Ques1-e modified with take_input and pattern printing function.

--- Ques1-e.cpp
+++ Ques1-e.cpp
@@ -2,31 +2,45 @@
 #include <stdlib.h>
 using namespace std;
 
+// to take the input from the user
+void take_input(int *n){
+    cout << "Enter a Number: ";
+    cin >> *n;
+    return ;
+}
+
+
+// function to print the fibonacci pattern
+void print_fibonacci_pattern(int no_of_terms){
+    long long int next_term = 0, previous_term = 1, swapping_term = 0;
+
+    //implementing the logic
+    for(int term = 0;  ; term++){
+
+        if(next_term > no_of_terms){
+            break;
+        }
+
+        cout << next_term << ' ';
+        previous_term +=  swapping_term;
+        swapping_term = next_term;
+        next_term = previous_term;
+
+    }
+    return;
+}
+
 int main(){
     char run_again = 'y';
     while(run_again == 'y'){
         system("cls");
 
         // having the input
-        cout << "Enter a Number: ";
-        int n; // according to the question variable name is given
-        cin >> n;
+        int n;
 
-        long long int next_term = 0, previous_term = 1, swapping_term = 0;
+        take_input(&n);
+        print_fibonacci_pattern(n);
 
-        //implementing the logic
-        for(int term = 0; next_term < n ; term++){
-
-            if(next_term > n){
-                break;
-            }
-
-            cout << next_term << ' ';
-            previous_term +=  swapping_term;
-            swapping_term = next_term;
-            next_term = previous_term;
-
-        }
         //asking to run again
         cout << endl << "Do you wanna run program again...??? (y/n) " << endl;
         cin >> run_again;
