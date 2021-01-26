6bf2cdf2839a834f8821c337e6e5f6c4fdee5f62
lalit-1729 <lakithakare54726@gmail.com>
2021-01-26 16:46:10 +0530

    Code cleaing and code refactoring is done & input validation feature is added.

--- Ques13-a.cpp
+++ Ques13-a.cpp
@@ -1,33 +1,81 @@
+//This program takes input from the user
+//Validates the input
+//And using the Recursion technique it prints the factorial
+
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
 
-//factorial function with recurrsion
-int factorial_of(long long int number){
+//factorial function with recursion
+int factorial_of(int number){
     if(number != 1){
         return number*factorial_of(number-1);
+    }
+    //factorial of 0 ,i.e 0! is 1
+    else if(number == 0){
+        return 1;
     }
     else{
         return 1;
     }
 }
 
-int main(){
-    char run_again = 'y';
+//takes the user input
+void take_input(string *user_input){
+    cout << "Enter a number: ";
+    cin >> *user_input;
+}
 
-    while(run_again == 'y'){
-        system("cls");
-
-        int input_number;
-        cout << "Enter a number: ";
-        cin >> input_number;
-
-        cout << "Factorial is " << factorial_of(input_number) << endl;
-
-        //asking to run again
-        cout << "Do you wanna run program again...??? (y/n) " << endl;
-        cin >> run_again;
-        cout << endl;
+//validates only non-negative integer
+bool is_input_valid(string user_input){
+    for(int i = 0; user_input[i] != '\0' ; i++){
+        // using the ACSII code for validation
+        if(user_input[i] < 48 || user_input[i] > 57){
+            return false;
+        }
     }
+    return true;
+}
+
+//asks user to rerun the program and returns a bool value accordingly
+bool want_to_run_again(){
+    char continue_program;
+    cout << "\nWould you like to run the program again? (y for yes)/(n for no)" << endl;
+    cin >> continue_program;
+
+    //input validation
+    while( !( continue_program == 'y' || continue_program == 'n' ) ){
+        cout << "Invalid Response, enter a valid one: ";
+        cin >> continue_program;
+    }
+
+    if(continue_program == 'y'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+int main(){
+    do{
+        system("cls");//clears the screen after every successful run
+        cout << "\n\n\t\t :: Factorial With Recursion :: \n\n";
+
+        string user_input;
+        take_input(&user_input);
+
+        //input validation
+        while(!is_input_valid(user_input)){
+            cout << "Invalid input, please try again with a valid one, ";
+            take_input(&user_input);
+        }
+
+        int number = stoi(user_input);// converting a valid input string to integer
+        cout << "Factorial is " << factorial_of(number) << endl;
+
+    }while(want_to_run_again() == true);
+
     return 0;
 }
