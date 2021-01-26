b2b3d56696d35eae23805603ffe5e3666225d74a
lalit-1729 <lakithakare54726@gmail.com>
2021-01-26 15:21:18 +0530

    In following file the code is cleaned up, code is refactored and input validation feature is added.

--- Ques15.cpp
+++ Ques15.cpp
@@ -1,33 +1,81 @@
+//This program take the input from the user
+//Validates it as a year and prints whether its a leap year or not
+
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
 
+//Takes the input from the user
+void take_input(string *user_input){
+    cout << "Enter a year: ";
+    cin >> *user_input;
+}
+
+//validating only non-negative integers
+bool is_input_valid(string user_input){
+    for(int i = 0; user_input[i] != '\0' ; i++){
+        // using the ACSII code for validation
+        if(user_input[i] < 48 || user_input[i] > 57){
+            return false;
+        }
+    }
+    return true;
+}
+
+//check for the leap year and displays the result
+void check_for_leap_year(int year){
+
+    if(year%100 == 0 && year%400 == 0){
+        cout << "This is a Leap year." << endl;
+        }
+    else if( year%4 ==0 && year%100 != 0 ){
+        cout << "This is a Leap year." << endl;
+    }
+    else{
+        cout << "This is not a leap year." << endl;
+    }
+    return;
+}
+
+//asks user to rerun the program and takes action accordingly
+bool want_to_run_again(){
+    char continue_program;
+    cout << "\nWould you like to run the program again? (y for yes)/(n for no)" << endl;
+    cin >> continue_program;
+
+    //input validation
+    while( !( continue_program == 'y' || continue_program == 'n' ) ){
+        cout << "Invalid Response, enter a valid one: ";
+        cin >> continue_program;
+    }
+
+    if(continue_program == 'y'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
 int main(){
 
-    char run_again = 'y';
+    do{
+        system("cls"); //clear the console after every successful run
+        cout << "\n\n\t\t :: Check For A Leap Year ::\n\n";
+        string user_input;
+        take_input(&user_input);
 
-    while(run_again == 'y'){
-        system("cls");
-
-        int year;
-        cout << "Enter the year: ";
-        cin >> year;
-
-        if(year%100 == 0 && year%400 == 0){
-            cout << "This is a Leap year." << endl;
-            }
-        else if( year%4 ==0 && year%100 != 0 ){
-            cout << "This is a Leap year." << endl;
-        }
-        else{
-            cout << "This is not a leap year." << endl;
+        //input validation
+        while(!is_input_valid(user_input)){
+            cout << "Invalid input, please try again with a valid one, ";
+            take_input(&user_input);
         }
 
-        //asking to run again
-        cout << "Do you wanna run program again...??? (y/n) " << endl;
-        cin >> run_again;
-        cout << endl;
-    }
+        int year = stoi(user_input);// converting a valid input string to integer
+        check_for_leap_year(year);
+
+    }while(want_to_run_again() == true);
 
     return 0;
 }
