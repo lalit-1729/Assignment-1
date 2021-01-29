bbb55586d756bab49ca3c96c110bb7c5e3186688
lalit-1729 <lakithakare54726@gmail.com>
2021-01-29 19:42:48 +0530

    Code is cleaned up & refactored, input validation feature is added.

--- Ques12.cpp
+++ Ques12.cpp
@@ -1,6 +1,27 @@
+//This program takes the user input
+//Validates the input
+//And Draws the pascal triangle
+
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+//Takes the user input
+void take_input(string *user_input){
+    cout << "Enter the number of rows: ";
+    cin >> *user_input;
+}
+
+//Validates only non-negative integer
+bool is_input_valid(string user_input){
+    for(int i = 0; user_input[i] != '\0' ; i++){
+        //Using the ASCII code for validation
+        if(user_input[i] < 48 || user_input[i] > 57){
+            return false;
+        }
+    }
+    return true;
+}
 
 // factorial funtion
 int factorial(int num){
@@ -22,29 +43,60 @@ int nCr(int n, int r){
     return factorial(n)/(factorial(r)*factorial(n-r));
 }
 
-int main(){
-    char run_again = 'y';
-    while(run_again == 'y'){
-        system("cls");
-        int rows;
-        cout << "Enter the number of rows: ";
-        cin >> rows;
-
-        for(int i = 0; i < rows ; i++){
-            for(int j = 1; j < rows-i ; j++ ){
-                    cout << " ";
-                }
-            //cout << "1" << endl;
-            for(int j = 0; j<=i;j++){
-                printf("%d ", nCr(i, j) );
+//Draws the pascal's triangle
+void draw_pascal_triangle(int rows){
+    for(int i = 0; i < rows ; i++){
+        //printing the spaces
+        for(int j = 1; j < rows-i ; j++ ){
+                cout << " ";
             }
-            cout << endl;
+        for(int j = 0; j<=i;j++){
+            printf("%d ", nCr(i, j) );
         }
-
-        // asking to run again
-        cout << "Do you wanna run program again...??? (y/n) " << endl;
-        cin >> run_again;
         cout << endl;
     }
+    return;
+}
+
+//Asking user to rerun and returning the bool value accordingly
+bool want_to_run_again(){
+    char continue_program;
+    cout << "\nWould you like to run the program again? (y for yes)/(n for no)" << endl;
+    cin >> continue_program;
+
+    //input validation, validates only lowercase 'y' & 'n'
+    while( !( continue_program == 'y' || continue_program == 'n' ) ){
+        cout << "Invalid Response, enter a valid one: ";
+        cin >> continue_program;
+    }
+
+    if(continue_program == 'y'){
+        return true;
+    }
+    else{
+        cout << "\n\n\t\t\t YOU JUST QUIT THE PROGRAM...!!!\n\n";
+        return false;
+    }
+}
+
+
+int main(){
+    do{
+        system("cls");
+        cout << "\n\n\t\t :: PASCAL'S TRIANGLE :: \n\n\n";
+        string user_input;
+        take_input(&user_input);
+
+        //input validation
+        while(is_input_valid(user_input) == false){
+            cout << "\nInvalid Input, please try again with a valid one:\n";
+            take_input(&user_input);
+        }
+        int rows = stoi(user_input);//string to integer conversion
+
+        draw_pascal_triangle(rows);
+
+    }while(want_to_run_again() == true);
+
     return 0;
 }
