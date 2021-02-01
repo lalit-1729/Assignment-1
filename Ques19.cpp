28abc65821f144e62b7cbc38b10c577ca5d74ec2
lalit-1729 <lakithakare54726@gmail.com>
2021-02-02 01:34:55 +0530

    Code is cleaned up and Refactored.

--- Ques19.cpp
+++ Ques19.cpp
@@ -1,57 +1,88 @@
-//program reads the text from the .txt file and also gives the number of the lines in the .txt files
+//program reads the text from the .txt file or similar files
+//gives the number of the lines in the .txt files
 
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
 
-// no of lines function
-int no_of_lines(string funciton_string){
-    int index = 0;
-    int no_of_lines = 0;
-    while(funciton_string[index] != '\0'){
-        if(funciton_string[index] == '\n'){
-            no_of_lines++;
-        }
-
-        index++;
+//Reads the text from the file
+string read_text_from_file(FILE *file){
+    // getting the first character
+    char single_character= fgetc(file);
+    string file_text;
+    // checking for the end of the file, EOF == End of file
+    while(single_character != EOF){
+        file_text += single_character;
+        single_character = fgetc(file);
     }
+    return file_text;
+}
 
-    return no_of_lines+1; // adding one for the last line
+//Asks to display the text from the file, and if yes then it displays it
+void display_text(string file_text){
+    char display_text;
+    cout << "would you like to display the text...?? (y for yes) / ( anything else for no)\n";
+    cin >> display_text;
+    if(display_text == 'y'){
+        cout << "The text of file is: \n\n---------------------------------------Start\n";
+        cout << file_text;
+        cout << "\n---------------------------------------End";
+    }
 }
 
 
+//no of lines function
+int count_no_of_lines(string funciton_string){
+    int no_of_lines = 0;
+    for(int i = 0;funciton_string[i] != '\0'; i++){
+        if(funciton_string[i] == '\n'){
+            no_of_lines++;
+        }
+    }
+    return no_of_lines+1; // adding one for the last line
+}
+
+//Asks the user to rerun and returns the bool value accordingly
+bool want_to_run_again(){
+    char continue_program;
+    cout << "\nWould you like to run the program again? (y for yes)/(n for no)" << endl;
+    cin >> continue_program;
+
+    //input validation
+    while( !( continue_program == 'y' || continue_program == 'n' ) ){
+        cout << "Invalid Response, enter a valid one: ";
+        cin >> continue_program;
+    }
+
+    if(continue_program == 'y'){
+        return true;
+    }
+    cout << "\n\t\tYou just Quit the program...!!\n\n";
+    return false;
+}
+
 int main(){
-    char run_again = 'y';
-    while(run_again == 'y'){
-        system("cls");
-        FILE *file;
+    do{
+        system("cls");//Clears the console window after every successful run
+        cout << "\n\n\t\t :: Counting No Of Lines In A File :: \n\n";
+
         char filename[50];
         cout << "Enter the name of the file: " << endl;
         cin >> filename;
+
+        FILE *file;
         file = fopen(filename, "r");
-        char c = fgetc(file); // getting the first character
-
-        string file_text;
-        while(c != EOF){ // checking for the end of the file, EOF == End of file
-            file_text += c;
-            c = fgetc(file); // getting the next character each time
+        if( file == NULL){
+            cout << "File not found, please try again:";
+        }
+        else{
+            string file_text = read_text_from_file(file);
+            display_text(file_text);
+            cout << "\n\n\nNumber of new lines in the file provided are: ";
+            cout << count_no_of_lines(file_text);
         }
 
-        char display_text;
-        cout << "Want to display the text...??? (y/n)\n";
-        cin >> display_text;
-        if(display_text == 'y'){
-            cout << "The text of file is: \n";
-            cout << file_text;
-        }
-
-        cout << "\n\n\nNumber of new lines in the file provided are: " << no_of_lines(file_text);
-
-        //asking to run again
-        cout << endl << endl <<"\nDo you wanna run program again...??? (y/n) " << endl;
-        cin >> run_again;
-        cout << endl;
-    }
+    }while(want_to_run_again() == true);
 
     return 0;
 }
