c0fc0b30f149d60e7f0c7b69beb04c026fd15d6c
lalit-1729 <lakithakare54726@gmail.com>
2021-02-09 15:51:18 +0530

    Uploading a correct code, earlier the code was uploaded although it was modified, after cleaning and refactoring.

--- Ques18.c
+++ Ques18.c
@@ -1,61 +1,100 @@
-// NOTE: the program is case sensitive
+//NOTE: the program is case sensitive
+//This program takes two input from the user and validates it
+//ANd prints whether they are anagram or not
 
 #include <stdio.h>
 #include <stdlib.h>
 
 // declaring the string_length function
 int string_length(char string1[]);
+void take_input(char *user_input, int string_number);
+void check_for_anagram(char *string1, char *string2);
+int is_string_valid(char *user_input);
+void ask_to_run_again(char *continue_program);
 
+
+//Main method starts here
 int main(){
-    char run_again = 'y';
-
-    while(run_again == 'y'){
+    char continue_program;
+    do{
         system("cls");
+        printf("\n\n\t\t :: CHECKING FOR ANAGRAM :: \n\n\n");
+        char input_string1[100000], input_string2[100000];
+        take_input(input_string1, 1);
+        //input validation
+        while(!is_string_valid(input_string1)){
+            if(!is_string_valid(input_string1)){
+                printf("Invalid input, please try again with a valid one:\n");
+                take_input(input_string1, 1);
+            }
+        }
 
+        take_input(input_string2, 2);
+        //input validation
+        while(!is_string_valid(input_string2)){
+            if(!is_string_valid(input_string2)){
+                printf("Invalid input, please try again with a valid one:\n");
+                take_input(input_string2, 2);
+            }
+        }
 
-        char input_string1[100000]; // assuming 100000 as max_length
-        char input_string2[100000];
-        printf("Enter the string1: ");
-        scanf("%s", &input_string1);
-        printf("Enter the String2: ");
-        scanf("%s", &input_string2);
-
-
-        // checking the length logic
-        int length_of_string1 = string_length(input_string1), length_of_string2 = string_length(input_string2);
+        int length_of_string1 = string_length(input_string1);
+        int length_of_string2 = string_length(input_string2);
         if(length_of_string1 != length_of_string2){
-            printf("The strings are not Anagram.");
+            printf("Not Anagram.");
             return 0;
         }
-
-        // implementing the anagram logic
         else{
-
-            int flag = 0;
-            for(int i= 0; i<length_of_string1; i++){
-                for(int j = 0; j< length_of_string1; j++ ){
-                    if(input_string1[i] == input_string2[j]){
-                        input_string2[j] = '.';
-                        flag++;
-                        break;
-                    }
-                }
-            }
-
-            if(flag == length_of_string1){
-                printf("\n\nAnagram\n");
-            }
-            else{
-                printf("\n\nNot Anagram\n");
-            }
+            check_for_anagram(input_string1, input_string2);
         }
 
-        printf("Do you wanna run program again....??? (y/n) ");
-        scanf(" %c", &run_again);
-        printf("\n");
+        ask_to_run_again(&continue_program);
+    }while(continue_program == 'y' || continue_program == 'Y');
+
+    printf("\n\n\t\t YOU JUST QUIT THE PROGRAM....!!\n\n");
+    return 0;
+}
+
+
+//Checks and prints the result of the anagrams
+void check_for_anagram(char *string1, char *string2){
+    int letters_matched = 0;
+    int string_size = string_length(string1);
+
+    for(int i= 0; i<string_size; i++){
+        for(int j = 0; j< string_size; j++ ){
+            if(*(string1 + i) == *(string2 + j)){
+                *(string2 + j) = '.';
+                letters_matched++;
+                break;
+            }
+        }
     }
 
-    return 0;
+    if(letters_matched == string_size){
+        printf("\n\nAnagram\n");
+    }
+    else{
+        printf("\n\nNot Anagram\n");
+    }
+}
+
+//Takes the input from the user
+void take_input(char *user_input, int string_number){
+    printf("Enter the string%d: ", string_number);
+    scanf(" %s", user_input);
+}
+
+//Validates the input as alphabetic string only
+int is_string_valid(char *user_input){
+    int string_size = string_length(user_input);
+    for(int i = 0; *(user_input+i) != '\0' ; i++){
+        // using the ACSII code for validation, 'a' = 97 & 'z' = 122
+        if(*(user_input + i) < 97 || *(user_input + i) > 122){
+            return 0;
+        }
+    }
+    return 1;
 }
 
 //defining the string length function
@@ -65,4 +104,21 @@ int string_length(char string1[]){
         string_len++;
     }
     return string_len;
+}
+
+//Asks user to run again and validates the input
+void ask_to_run_again(char *continue_program){
+    printf("Would you like to run the program again...??? (y for yes) / (n for no)\n");
+    scanf(" %c", continue_program);
+
+    //Validating only 'y' & 'n'
+    if(!(*continue_program == 'y' || *continue_program == 'n' ||
+          *continue_program == 'Y' || *continue_program == 'N')){
+
+        while(!(*continue_program == 'y' || *continue_program == 'n' ||
+                 *continue_program == 'Y' || *continue_program == 'N')){
+            printf("Invalid ibput please try again with a valid input:\n");
+            scanf(" %c", continue_program);
+        }
+    }
 }
