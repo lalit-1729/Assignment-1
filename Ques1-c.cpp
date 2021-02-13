adf623c8e8cbf345732ffd6e320a229b53686ae1
lalit-1729 <lakithakare54726@gmail.com>
2021-02-13 23:39:39 +0530

    Code cleaned and fixed a bug in the 'run again' loop

--- Ques1-c.cpp
+++ Ques1-c.cpp
@@ -1,6 +1,43 @@
+//This program takes the input from the user and validates it
+//And then prints pattern similar to shown below
+//--------------+
+// for n = 6    |
+//              |
+//      $       |
+//     $$$      |
+//    $$$$$     |
+//   $$$$$$$    |
+//  $$$$$$$$$   |
+// $$$$$$$$$$$  |
+//--------------+
+
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+void refresh_screen(){
+    system("cls");
+    cout << "\n\n\t\tSample pattern, for m = 4:\n\t\t      :    *\n\t\t\t  ***\n\t\t\t *****\n\t\t\t******* :\n\n"; // Program introduction
+}
+
+int string_length(string function_string){
+    int string_len = 0;
+    for(string_len = 0 ;function_string[string_len] != '\0'; string_len++);  //Running till the null character
+    return string_len;
+}
+
+bool compare_strings(string string1, string string2){
+    if(string_length(string1) != string_length(string2))
+        return false;
+    else{
+        for(int i = 0; i<string_length(string1) ; i++){
+            if(string1[i] != string2[i]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 void take_input(string *user_input){
     cout << "Enter the number of rows of patterns: "<< endl;
@@ -9,10 +46,9 @@ void take_input(string *user_input){
 
 bool is_input_valid(string user_input){
     for(int i = 0; user_input[i] != '\0' ; i++){
-        // using the ACSII code for validation
-        if(user_input[i] < 48 || user_input[i] > 57){
+        // using the ACSII to look for only numeric character
+        if(user_input[i] < 48 || user_input[i] > 57)
             return false;
-        }
     }
     return true;
 }
@@ -20,54 +56,47 @@ bool is_input_valid(string user_input){
 // function to print the pattern
 void print_pattern(int no_of_rows){
     for(int row = 0; row < no_of_rows ; row++){
-        for(int space = 1; space < no_of_rows - row ; space++ ){
+        for(int space = 1; space < no_of_rows - row ; space++ )
             cout << " ";
-        }
-        for(int character = 0; character < (2*row + 1) ; character++){
+        for(int character = 0; character < (2*row + 1) ; character++)
             cout << "$";
-        }
         cout << endl;
     }
     return;
 }
 
-//asks user to rerun the program and returns bool value accordingly
-bool want_to_run_again(){
-    char continue_program;
+//asks user to rerun the program
+void want_to_run_again(string *user_input){
     cout << "\nWould you like to run the program again? (y for yes)/(n for no)" << endl;
-    cin >> continue_program;
+    cin >> *user_input;
 
-    //input validation
-    while( !( continue_program == 'y' || continue_program == 'n' ) ){
+    //input validation, only y, Y, N, n are allowed
+    while( !( compare_strings(*user_input, "y") || compare_strings(*user_input, "n") ||
+             compare_strings(*user_input, "Y") || compare_strings(*user_input, "N")) ){
         cout << "Invalid Response, enter a valid one: ";
-        cin >> continue_program;
-    }
-
-    if(continue_program == 'y'){
-        return true;
-    }
-    else{
-        cout << "\n\nYOU JUST QUIT THE PROGRAM...!!!\n";
-        return false;
+        cin >> *user_input;
     }
 }
 
-int main(){
+int main(void){
+    string continue_program;
+
     do{
-        system("cls");
-        cout << "\n\nExample pattern, for m = 4: \n\t\t ::   *  \n\t\t     ***\n\t\t    *****\n\t\t   ******* ::\n\n";
+        refresh_screen();
         string user_input;
         take_input(&user_input);
 
         //input validation
-        while(is_input_valid(user_input) == false){
+        while(!is_input_valid(user_input)){
             cout << "Invalid Input, please try again with a valid one:\n";
             take_input(&user_input);
         }
 
-        int input_num = stoi(user_input);
-        print_pattern(input_num);
+        int no_of_rows = stoi(user_input);//Converting the valid string to integer
+        print_pattern(no_of_rows);
 
-    }while(want_to_run_again() == true);
-    return 0;
+        want_to_run_again(&continue_program);
+
+    }while( continue_program[0] == 'y' || continue_program[0] == 'Y' );
+    cout << "\n\n\t\tYOU HAVE QUIT THE PROGRAM....!!!\n";
 }
