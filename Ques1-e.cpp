669ae74488801cbdf74134fe6d7008ef516ff39a
lalit-1729 <lakithakare54726@gmail.com>
2021-02-13 23:40:20 +0530

    Code cleaned and fixed a bug in the 'run again' loop

--- Ques1-e.cpp
+++ Ques1-e.cpp
@@ -6,78 +6,91 @@
 #include <stdlib.h>
 using namespace std;
 
-//to take the user input
-void take_input(string *user_input){
-    cout << "Enter the number of rows of patterns: "<< endl;
-    cin >> *user_input;
+void refresh_screen(){
+    system("cls"); //To clear the screen
+    cout << "\n\n\t\t:: Printing The Fibonacci Sequence Less Than m ::";
+    cout << "\n\t\t   Sample pattern, for m = 6 ::: 0 1 1 2 3 5\n\n";
 }
 
-//Validates only non-negative integers only
-bool is_input_valid(string user_input){
-    for(int i = 0; user_input[i] != '\0' ; i++){
-        // using the ACSII code for validation
-        if(user_input[i] < 48 || user_input[i] > 57){
-            return false;
+int string_length(string function_string){
+    int string_len = 0;
+    for(string_len = 0 ;function_string[string_len] != '\0'; string_len++);  //Running till the null character ('\0', the terminating character)
+    return string_len;
+}
+
+bool compare_strings(string string1, string string2){
+    if(string_length(string1) != string_length(string2))
+        return false;
+    else{
+        for(int i = 0; i<string_length(string1) ; i++){
+            if(string1[i] != string2[i])
+                return false;
         }
     }
     return true;
 }
 
+void take_input(string *user_input){
+    cout << "Enter the number of rows of patterns: "<< endl;
+    cin >> *user_input;
+}
 
-// function to print the fibonacci pattern
+bool is_input_valid(string user_input){
+    for(int i = 0; user_input[i] != '\0' ; i++){
+        // using the ACSII code to look for only numeric character
+        if(user_input[i] < 48 || user_input[i] > 57)
+            return false;
+    }
+    return true;
+}
+
+// function to print the Fibonacci pattern
 void print_fibonacci_pattern(int no_of_terms){
     int next_term = 0, previous_term = 1, temp_term = 0;
     for(int term = 0;  ; term++){
-        if(next_term > no_of_terms){
+        if(next_term > no_of_terms)
             break;
-        }
 
         cout << next_term << ' ';
         previous_term +=  temp_term;
         temp_term = next_term;
         next_term = previous_term;
-
     }
     return;
 }
 
-//asks user to rerun the program and returns bool value accordingly
-bool want_to_run_again(){
-    char continue_program;
+//asks user to rerun the program
+void want_to_run_again(string *user_input){
     cout << "\nWould you like to run the program again? (y for yes)/(n for no)" << endl;
-    cin >> continue_program;
+    cin >> *user_input;
 
     //input validation
-    while( !( continue_program == 'y' || continue_program == 'n' ) ){
+    while( !( compare_strings(*user_input, "y") || compare_strings(*user_input, "n") ||
+             compare_strings(*user_input, "Y") || compare_strings(*user_input, "N")) ){
         cout << "Invalid Response, enter a valid one: ";
-        cin >> continue_program;
-    }
-
-    if(continue_program == 'y'){
-        return true;
-    }
-    else{
-        cout << "\n\nYOU JUST QUIT THE PROGRAM...!!!\n";
-        return false;
+        cin >> *user_input;
     }
 }
 
-int main(){
+int main(void){
+    string continue_program;
+
     do{
-        system("cls");
-        cout << "\n\nExample pattern, for m = 5: \n\t\t :: 0 1 1 2 3 ::\n\n";
+        refresh_screen();
         string user_input;
         take_input(&user_input);
 
         //input validation
-        while(is_input_valid(user_input) == false){
+        while(!is_input_valid(user_input)){
             cout << "Invalid Input, please try again with a valid one:\n";
             take_input(&user_input);
         }
 
-        int n = stoi(user_input);
+        int n = stoi(user_input); //Converting the valid user input to integer
         print_fibonacci_pattern(n);
 
-    }while(want_to_run_again() == true);
-    return 0;
+        want_to_run_again(&continue_program);
+
+    }while( continue_program[0] == 'y' || continue_program[0] == 'Y' );
+    cout << "\n\n\t\tYOU HAVE QUIT THE PROGRAM....!!!\n";
 }
