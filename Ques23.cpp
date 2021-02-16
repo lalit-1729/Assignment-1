b8c2468b81e2224f62468aa0219ddcf11111f8cb
lalit-1729 <lakithakare54726@gmail.com>
2021-02-16 23:30:32 +0530

    Code cleaned and refactored & fixed few bugs in the game play-again loop.

--- Ques23.cpp
+++ Ques23.cpp
@@ -1,110 +1,118 @@
 //TIC-TAC-TOE game
+//Here, the game is played using a board which has 1-9 marks on it
+//conveying the position of the X & O, that is to be marked on the board
+//And the winner is declared based on the moves made
 
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
 
-char array[9];
+char board_array[9];
 int moves_made[9];
 
-//Takes the input from the user
-void take_input(int player_number, string *user_input){
-    cout << "\n\nPlayer " << player_number << " turn, make your move: " << endl;
-    cin >> *user_input;
+void refresh_screen(){
+    system("cls"); //To clear the Console window
+    cout << "\n\n\t\t :: TIC-TAC-TOE :: \n\n\n";
 }
 
-//Validates the integers only
-bool is_input_numeric(string user_input){
-    for(int i = 0; user_input[i] != '\0' ; i++){
-        // using the ACSII code for validation
-        if(user_input[i] < 48 || user_input[i] > 57){
-            return false;
+int string_length(string user_input){
+    int string_size = 0;
+    while(user_input[string_size] != '\0'){
+         string_size++;
+    }
+    return string_size;
+}
+
+bool compare_strings(string string1, string string2){ //Similar to that of 'strcmp' function of string.h
+    if(string_length(string1) != string_length(string2))
+        return false;
+    else{
+        for(int i = 0; i<string_length(string1) ; i++){
+            if(string1[i] != string2[i])
+                return false;
         }
     }
     return true;
 }
 
-// Move range Validator
-bool is_move_range_valid(int MOVE){
-
-    if(MOVE < 0 || MOVE > 9){
-        return false;
-    }
-    else{
-        return true;
-    }
+void take_input(short int player_number, string *user_input){
+    printf("\n\nPlayer %d turn, make your move: ", player_number);
+    cin >> *user_input;
 }
 
-// repeated move vallidator
-bool is_move_repeated(int MOVE){
-    for(int i = 0; i<9 ; i++){
-        if(moves_made[i] == MOVE){
+bool is_input_numeric(string user_input){
+    for(int i = 0; user_input[i] != '\0' ; i++){
+        // using the ACSII code for validation
+        if(user_input[i] < 48 || user_input[i] > 57)
             return false;
-        }
+    }
+
+    return true;
+}
+
+//Here we are checking whether the user input is
+//Between 1-9 only
+bool is_move_range_valid(int move){
+    if(move < 0 || move > 9)
+        return false;
+    else
+        return true;
+}
+
+bool is_move_repeated(int move){
+    for(int i = 0; i<9 ; i++){
+        if(moves_made[i] == move)
+            return false;
+
     }
 
     return true;
 }
 
 //Validates the input using the above three function
-void input_validator(string *user_input, int player_number){
-    while(is_input_numeric(*user_input) == false ||
-           is_move_range_valid(stoi(*user_input)) == false ||
-           is_move_repeated(stoi(*user_input)) == false )
+void validating_input(string *user_input, int player_number){
+    while(!is_input_numeric(*user_input)||
+           !is_move_range_valid(stoi(*user_input))||
+          !is_move_repeated(stoi(*user_input)) )
         {
-        cout << "Invalid Move: ";
+        cout << "Invalid move: ";
         take_input(player_number, user_input);
     }
 }
 
-// To Check whether a player won or not
+//To Check whether a player won or not
+//ALl the possible winning combination are taken here
 bool check_for_win(){
 
-    if(array[0] == array[1] && array[1] == array[2]){
+    if(board_array[0] == board_array[1] && board_array[1] == board_array[2])
         return true;
-    }
-
-    else if(array[3] == array[4] && array[4] == array[5]){
+    else if(board_array[3] == board_array[4] && board_array[4] == board_array[5])
         return true;
-    }
-
-    else if(array[6] == array[7] && array[7] == array[8]){
+    else if(board_array[6] == board_array[7] && board_array[7] == board_array[8])
         return true;
-    }
-
-    else if(array[0] == array[3] && array[3] == array[6]){
+    else if(board_array[0] == board_array[3] && board_array[3] == board_array[6])
         return true;
-    }
-
-    else if(array[1] == array[4] && array[4] == array[7]){
+    else if(board_array[1] == board_array[4] && board_array[4] == board_array[7])
         return true;
-    }
-
-    else if(array[2] == array[5] && array[5] == array[8]){
+    else if(board_array[2] == board_array[5] && board_array[5] == board_array[8])
         return true;
-    }
-
-    else if(array[0] == array[4] && array[4] == array[8]){
+    else if(board_array[0] == board_array[4] && board_array[4] == board_array[8])
         return true;
-    }
-
-    else if(array[6] == array[4] && array[4] == array[2]){
+    else if(board_array[6] == board_array[4] && board_array[4] == board_array[2])
         return true;
-    }
-    else{
+    else
         return false;
-    }
 }
 
-//for new game equating all moves made to -1
+//the following function will refresh all the moves
+//and the board for the new game
 void refresh_moves_n_board(){
     for(int index = 0; index<9 ; index++){
-        array[index] = char('1'+index);
+        board_array[index] = char('1'+index);
         moves_made[index] = -1;
     }
 }
 
-//  Drawing the board
 void draw_board(char arr[9]){
     system("cls");
     cout << "\n\n\n\n\t\t\t\t\tTIC-TAC-TOE\n\n\n\nPlayer 1 - X & Player 2 - O\n\n\n";
@@ -119,76 +127,72 @@ void draw_board(char arr[9]){
     cout << "     |     |     " << endl;
 }
 
-//asks user to play again and takes action accordingly
-bool want_to_run_again(){
-    char continue_program;
+//asks user to rerun the program
+void want_to_play_again(string *user_input){
     cout << "\nWould you like to play again? (y for yes)/(n for no)" << endl;
-    cin >> continue_program;
+    cin >> *user_input;
 
-    //input validation
-    while( !( continue_program == 'y' || continue_program == 'n' ) ){
+    //input validation, only y, n, Y, N are allowed
+    while( !( compare_strings(*user_input, "y") || compare_strings(*user_input, "n") ||
+             compare_strings(*user_input, "Y") || compare_strings(*user_input, "N")) ){
         cout << "Invalid Response, enter a valid one: ";
-        cin >> continue_program;
-    }
-
-    if(continue_program == 'y'){
-        return true;
-    }
-    else{
-        cout << "\n\n\t\t YOU JUST QUIT THE GAME...!!!\n\n";
-        return false;
+        cin >> *user_input;
     }
 }
 
 
 int main(){
+    string continue_program;
 
     do{
         refresh_moves_n_board();
 
         int num = 8;
-        bool is_winner_found = false;
+        string game_status;
         string user_input;
+
         do{
-            draw_board(array);
+            draw_board(board_array);
+
             // player 1 turn
             if(num%2 == 0){
                 take_input(1, &user_input);
-                input_validator(&user_input, 1);
+                validating_input(&user_input, 1);
 
-                int move = stoi(user_input);
-                moves_made[num] = move;//updating the moves made
-                array[move-1] = 'X';//updating the board
+                int move = stoi(user_input);//string to integer conversion
+                moves_made[num] = move;     //updating the moves made
+                board_array[move-1] = 'X';  //updating the board
 
                 if(check_for_win()){
-                    draw_board(array);
-                    cout << "\n\n*****************Player 1 won the game*****************\n\n";
-                    is_winner_found = true;
+                    game_status = "Player 1 won the game";
                     break;
                 }
             }
+
             // player 2 turn
             else{
                 take_input(2, &user_input);
-                input_validator(&user_input, 2);
+                validating_input(&user_input, 2);
 
-                int move = stoi(user_input);
-                moves_made[num] = move;//updating the moves made
-                array[move - 1] = 'O';//updating the board
+                int move = stoi(user_input);//string to integer conversion
+                moves_made[num] = move;     //updating the moves made
+                board_array[move - 1] = 'O';//updating the board
 
                 if(check_for_win()){
-                    draw_board(array);
-                    cout << "\n\n*****************Player 2 won the game*****************\n\n";
-                    is_winner_found = true;
+                    game_status = "Player 2 won the game";
                     break;
                 }
             }
         }while(num--);
 
-        if(is_winner_found == false){
-            draw_board(array);
-            cout << "\n\n\nThe Game is Tied\n";
-        }
-    }while(want_to_run_again() == true);
-    return 0;
+        draw_board(board_array);
+        if(!compare_strings(game_status, "Player 1 won the game") && !compare_strings(game_status, "Player 2 won the game") )
+            game_status = "Game Tied";
+
+        cout << "\n\n***************************" << game_status << "***************************\n\n";
+
+        want_to_play_again(&continue_program);
+
+    }while( continue_program[0] == 'y' || continue_program[0] == 'Y' );
+    cout << "\n\n\t\tYOU HAVE QUIT THE PROGRAM....!!!\n";
 }
