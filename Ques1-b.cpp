8d65593d82e43f38a47f40965cf2489bd2642c4d
lalit-1729 <lakithakare54726@gmail.com>
2021-02-20 02:30:21 +0530

    Code Cleaned

--- Ques1-b.cpp
+++ Ques1-b.cpp
@@ -17,16 +17,18 @@ using namespace std;
 
 void refresh_screen(){
     system("cls");
-    cout << "\n\n\t\tSample pattern, for m = 4:\n\t\t      :    $\n\t\t\t  $ $\n\t\t\t $ $ $\n\t\t\t$ $ $ $ :\n\n"; // Program introduction
+    cout << "\n\n\t\t :: Pattern Printing ::\n\t\tSample pattern, for m = 4:\n\t\t      :    $\n\t\t\t  $ $\n\t\t\t $ $ $\n\t\t\t$ $ $ $ :\n\n"; // Program introduction
 }
 
 int string_length(string function_string){
     int string_len = 0;
-    for(string_len = 0 ;function_string[string_len] != '\0'; string_len++);  //Running till the null character
+    for(string_len = 0 ;function_string[string_len] != '\0'; string_len++);  //Running till the null character i.e. till '\0' , the terminating character
     return string_len;
 }
 
-bool compare_strings(string string1, string string2){ //Similar to that of 'strcmp' function of string.h
+/* Similar to that of 'strcmp' function of string.h, here we are comparing every single character of both string
+   this function will will used later in the program run-again loop for input validation */
+bool compare_strings(string string1, string string2){
     if(string_length(string1) != string_length(string2))
         return false;
     else{
@@ -44,6 +46,10 @@ void take_input(string *user_input){
     cin >> *user_input;
 }
 
+/* The no of rows can't be negative or fractional,
+   therefore for the validation of the of the input this function is used,
+   this function will take string as a argument check each characters ASCII code is in range or not
+   and will return the bool value accordingly */
 bool is_input_valid(string user_input){
     for(int i = 0; user_input[i] != '\0' ; i++){
         // using the ACSII code to find valid numeric character
@@ -54,12 +60,17 @@ bool is_input_valid(string user_input){
     return true;
 }
 
+/*This function will print the pattern as shown in the intro of the this code,
+  according to the no of rows entered by the user*/
 void print_pattern(int no_of_rows){
     for(int row = 0; row <= no_of_rows ; row++){
-        for(int space = 0; space < no_of_rows - row ; space++ )
+        //Printing the spaces, required to keep triangle (pattern) symmetric
+        for(int space = 0; space < no_of_rows - row ; space++ ){
             cout << " ";
-        for(int j = 0; j < row ; j++)
-            cout << "$ ";
+        }
+        for(int j = 0; j < row ; j++){
+            cout << "$ "; //The pattern items with spaces between them
+        }
         cout << endl;
     }
 }
