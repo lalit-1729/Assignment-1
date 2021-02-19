dd3b79d0c6cfc61a9aad9d28262800f927b9072d
lalit-1729 <lakithakare54726@gmail.com>
2021-02-20 02:28:43 +0530

    Code Cleaned

--- Ques1-a.cpp
+++ Ques1-a.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 void refresh_screen(){
     system("cls");
-    cout << "\n\n\t\tSample pattern, for m = 4:\n\t\t      : *\n\t\t\t**\n\t\t\t***\n\t\t\t**** :\n\n"; // Program introduction
+    cout << "\n\n\t\t :: Pattern Printing ::\n\t\tSample pattern, for m = 4:\n\t\t      : *\n\t\t\t**\n\t\t\t***\n\t\t\t**** :\n\n"; // Program introduction
 }
 
 int string_length(string function_string){
@@ -27,7 +27,9 @@ int string_length(string function_string){
     return string_len;
 }
 
-bool compare_strings(string string1, string string2){ //Similar to that of 'strcmp' function of string.h
+/* Similar to that of 'strcmp' function of string.h, here we are comparing every single character of both string
+   this function will will used later in the program run-again loop for input validation */
+bool compare_strings(string string1, string string2){
     if(string_length(string1) != string_length(string2))
         return false;
     else{
@@ -45,9 +47,13 @@ void take_input(string *user_input){
     cin >> *user_input;
 }
 
+/* The no of rows can't be negative or fractional,
+   therefore for the validation of the of the input this function is used,
+   this function will take string as a argument to check each characters ASCII code is in range or not
+   and will return the bool value accordingly */
 bool is_input_valid(string user_input){
     for(int i = 0; user_input[i] != '\0' ; i++){
-        // using the ACSII code for validation
+        // using the ACSII code for validation, ASCII code for '0' = 48 and '9' = 57
         if(user_input[i] < 48 || user_input[i] > 57){
             return false;
         }
@@ -55,7 +61,8 @@ bool is_input_valid(string user_input){
     return true;
 }
 
-// function to print the pattern
+/* this function will print the pattern as shown in the intro of this code,
+   According to the no of the given by the user*/
 void print_pattern(int no_of_rows){
     for(int row = 0; row<no_of_rows ; row++){
         for(int character = 0; character<=row ; character++){
@@ -70,7 +77,7 @@ void want_to_run_again(string *user_input){
     cout << "\nWould you like to run the program again? (y for yes)/(n for no)" << endl;
     cin >> *user_input;
 
-    //input validation
+    //input validation, will allow only y, n, Y & N as valid input
     while( !( compare_strings(*user_input, "y") || compare_strings(*user_input, "n") ||
              compare_strings(*user_input, "Y") || compare_strings(*user_input, "N")) ){
         cout << "Invalid Response, enter a valid one: ";
